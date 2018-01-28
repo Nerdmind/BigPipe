c6bd37658cdae7e1dcd3ff4664c7e8a84a77bba7
Thomas Lange <code@nerdmind.de>
2018-01-28 01:58:48 +0100

    Remove superfluous item from pattern array
    The string "\r\n" within the search array is superfluous because it is already covered by the single "\r" and "\n".

--- include/functions.php
+++ include/functions.php
@@ -3,6 +3,6 @@
 # Remove line breaks and tabs from a string
 #===============================================================================
 function removeLineBreaksAndTabs($string) {
-	return str_replace(["\r\n", "\r", "\n", "\t"], NULL, $string);
+	return str_replace(["\r", "\n", "\t"], NULL, $string);
 }
 ?>
